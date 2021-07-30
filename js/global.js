863372329e8b614d2b91b6ac5722442e69573259
MrPabloVS <pablovazquezsoldano@gmail.com>
2021-07-30 17:19:02 -0300

    ingredientes constantes

--- js/global.js
+++ js/global.js
@@ -4,7 +4,17 @@ const cool = ["Not Cool", "Less Cool", "Cool", "Extra Cool"];
 const sweet = ["Not Sweet", "Less Sweet", "Sweet", "Extra Sweet"]
 const bitter = ["Not Bitter", "Less Bitter", "Bitter", "Extra Biter"]
 
-
+//Ingredientes
+const Coffee = "Coffee"
+const GreenTea = "Green Tea"
+const Tea = "Tea"
+const Chocolate = "Chocolate"
+const Milk = "Milk"
+const Ginger = "Ginger"
+const Mint = "Mint"
+const Lemon = "Lemon"
+const Honey = "Honey"
+const Cinammon = "Cinammon"
 
 //Constructor
 class receta {
--- js/main.js
+++ js/main.js
@@ -1,36 +1,4 @@
 
-//Declaracion de Variables de entrada
-/* let warmInput = "";
-let coolInput = "";
-let sweetInput = "";
-let bitterInput = ""; */
-
-/* //Arrays 
-const warm = ["Not Warm", "Less Warm", "Warm", "Extra Warm"];
-const cool = ["Not Cool", "Less Cool", "Cool", "Extra Cool"];
-const sweet = ["Not Sweet", "Less Sweet", "Sweet", "Extra Sweet"]
-const bitter = ["Not Bitter", "Less Bitter", "Bitter", "Extra Biter"]
-
-//Constructor
-class receta {
-    constructor(base, primary, secondary) {
-        this.base = base;
-        this.primary = primary;
-        this.secondary = secondary;
-    }
-} */
-
-
-//Armar recetas
-/* const receta2 = new receta("Tea", "Milk", "Cinammon")
-const receta3 = new receta("Tea", "Tea", "Milk")
-const receta4 = new receta("Tea", "Tea", "Lemon")
-const receta5 = new receta("Tea", "Mint", "Tea")
-const receta6 = new receta("Tea", "Tea", "Mint")
-const receta7 = new receta("Tea", "Cinammon", "Honey")
-const receta8 = new receta("Milk", "Cinammon", "Cinammon") */
-
-
 
 const salidaHTML = document.getElementById("pSalida")
 
